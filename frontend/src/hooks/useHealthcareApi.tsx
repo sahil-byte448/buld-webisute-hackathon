fae9823254217a633b14599ed255106f51080e3b
sahil-byte448 <202201021.sahilgps@student.xavier.ac.in>
2024-09-23 00:01:07 +0530

    trv

--- frontend/src/hooks/useHealthcareApi.tsx
+++ frontend/src/hooks/useHealthcareApi.tsx
@@ -53,7 +53,7 @@ export const useHealthcareApi = () => {
   };
 
   const getPatientData = async (patientId: string) => {
-    const endpoint = `https://gateway-api.kalp.studio/v1/contract/kalp/query/ZNd9fzZRIFt5o4yOFIp9E7BQxxomqioz1727028811273/Init`;
+    const endpoint = `https://gateway-api.kalp.studio/v1/contract/kalp/query/ZNd9fzZRIFt5o4yOFIp9E7BQxxomqioz1727028811273/GetPatientData`;
     const args = { patientId };
     return callApi(endpoint, args);
   };
